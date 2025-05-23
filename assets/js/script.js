19af42a07c80dffd97b6b0fdd635d44843771c04
hussain <gb12sarkar@gmail.com>
2025-05-24 02:04:53 +0500

    footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,45 @@
 const navLinks = document.querySelectorAll('#navList li a');
+const navList = document.getElementById('navList');
 
 navLinks.forEach(link => {
   link.addEventListener('click', function () {
-    // Remove active from all
     navLinks.forEach(l => l.classList.remove('active'));
-    // Add active to clicked one
     this.classList.add('active');
+    navList.classList.remove('show'); // hide menu after click
   });
 });
+
+
+const wrapper = document.getElementById('sliderWrapper');
+const track = document.getElementById('sliderTrack');
+
+let scrollAmount = 310; // image width + gap
+
+wrapper.addEventListener('click', (e) => {
+  const clickX = e.clientX;
+  const middle = window.innerWidth / 2;
+
+  if (clickX < middle) {
+    // click left
+    wrapper.scrollLeft -= scrollAmount;
+    if (wrapper.scrollLeft <= 0) {
+      wrapper.scrollLeft = track.scrollWidth / 2;
+    }
+  } else {
+    // click right
+    wrapper.scrollLeft += scrollAmount;
+    if (wrapper.scrollLeft >= track.scrollWidth / 2) {
+      wrapper.scrollLeft = 0;
+    }
+  }
+});
+
+// Auto-scroll (optional)
+function autoScroll() {
+  wrapper.scrollLeft += 1;
+  if (wrapper.scrollLeft >= track.scrollWidth / 2) {
+    wrapper.scrollLeft = 0;
+  }
+  requestAnimationFrame(autoScroll);
+}
+autoScroll();
